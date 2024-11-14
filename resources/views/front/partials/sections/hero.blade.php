a4fcb9bf360cbe2539b26389e320660378d3904e
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-11-14 23:07:43 +0300

    new content added

--- resources/views/front/partials/sections/hero.blade.php
+++ resources/views/front/partials/sections/hero.blade.php
@@ -1,33 +1,82 @@
 <section class="px-3">
     <div class="container max-w-screen-xl mx-auto">
-        <div class="relative">
-            <img src="{{ asset('front/assets/images/hero.jpg') }}" alt="hero">
+        <a href="#">
+            <div class="relative">
+                <img src="{{ asset('front/assets/images/hero.jpg') }}" alt="hero">
 
-            <span class="absolute top-5 left-5 bg-white px-3 py-1 flex items-center gap-2 shadow-lg font-bold"> <span
-                    class="h-3 w-3 bg-red-600 inline-block rounded-full "></span>
-                Live
-                Updates</span>
+                <span class="absolute top-5 left-5 bg-white px-3 py-1 flex items-center gap-2 shadow-lg font-bold"> <span
+                        class="h-3 w-3 bg-red-600 inline-block rounded-full "></span>
+                    Live
+                    Updates</span>
 
-        </div>
-        <div class="flex sm:justify-between flex-wrap items-center pt-5">
-            <div class="flex gap-2">
-                <button class="bg-blue-600 text-white px-3 py-2 rounded-xl text-sm">CULTURE</button>
-                <button class="bg-blue-600 text-white px-3 py-2 rounded-xl text-sm">WADE WARREN</button>
             </div>
-            <div class="flex gap-3 items-center">
-                <span class="text-sm">Sep 9, 2024</span>
-                <span class="text-xl">-</span>
-                <span class="text-sm"> 06 Minute</span>
+            <div class="flex sm:justify-between flex-wrap items-center pt-5">
+                <div class="flex gap-2">
+                    <button class="bg-blue-600 text-white px-3 py-2 rounded-xl text-sm">CULTURE</button>
+                    <button class="bg-blue-600 text-white px-3 py-2 rounded-xl text-sm">WADE WARREN</button>
+                </div>
+                <div class="flex gap-3 items-center">
+                    <span class="text-sm">Sep 9, 2024</span>
+                    <span class="text-xl">-</span>
+                    <span class="text-sm"> 06 Minute</span>
+                </div>
             </div>
-        </div>
-        <div class="flex sm:justify-between flex-wrap items-start pt-5">
-            <div class="text-4xl sm:w-[75%] w-full mb-4 font-bold">
-                A deep dive into the influence of cultural movements on contemporary society
+            <div class="flex sm:justify-between flex-wrap items-start pt-5">
+                <div class="text-4xl sm:w-[75%] w-full mb-4 font-bold">
+                    A deep dive into the influence of cultural movements on contemporary society
+                </div>
+                <div class="flex gap-2 items-center cursor-pointer">
+                    <span class="text-sm"> Read Article</span> <i
+                        class="fa-solid fa-arrow-right border border-solid border-gray-950 rounded-full p-1"></i>
+                </div>
             </div>
-            <div class="flex gap-2 items-center cursor-pointer">
-                <span class="text-sm"> Read Article</span> <i
-                    class="fa-solid fa-arrow-right border border-solid border-gray-950 rounded-full p-1"></i>
-            </div>
+        </a>
+
+        <div class="flex lg:justify-between flex-wrap gap-5 pt-5 ">
+            <a href="#" class="hero-box">
+                <img src="{{ asset('front/assets/images/top-img1.jpg') }}" alt="news">
+                <div class="flex items-center gap-2">
+                    <span class="text-sm">World News</span>
+                    <span class="text-xl">-</span>
+                    <span class="text-sm">Sep 9, 2024</span>
+                </div>
+                <div>
+                    The effects of geopolitical shifts on global security
+                </div>
+            </a>
+            <a href="#" class="hero-box">
+                <img src="{{ asset('front/assets/images/top-img2.jpg') }}" alt="news">
+                <div class="flex items-center gap-2">
+                    <span class="text-sm">Culture</span>
+                    <span class="text-xl">-</span>
+                    <span class="text-sm">Sep 9, 2024</span>
+                </div>
+                <div>
+                    Affect the integrity and future of professional sports
+                </div>
+            </a>
+            <a href="#" class="hero-box">
+                <img src="{{ asset('front/assets/images/top-img3.jpg') }}" alt="news">
+                <div class="flex items-center gap-2">
+                    <span class="text-sm">Technology</span>
+                    <span class="text-xl">-</span>
+                    <span class="text-sm">Sep 9, 2024</span>
+                </div>
+                <div>
+                    Strategies for success in a competitive landscape
+                </div>
+            </a>
+            <a href="#" class="hero-box">
+                <img src="{{ asset('front/assets/images/top-img4.jpg') }}" alt="news">
+                <div class="flex items-center gap-2">
+                    <span class="text-sm">Bussiness</span>
+                    <span class="text-xl">-</span>
+                    <span class="text-sm">Sep 9, 2024</span>
+                </div>
+                <div>
+                    The effects of geopolitical shifts on global security
+                </div>
+            </a>
         </div>
     </div>
 </section>
