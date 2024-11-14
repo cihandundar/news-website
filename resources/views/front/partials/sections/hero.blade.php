7b3f1636cc46fe19174c861892eac37eef9743d2
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-11-14 22:19:58 +0300

    hero section added

--- resources/views/front/partials/sections/hero.blade.php
+++ resources/views/front/partials/sections/hero.blade.php
@@ -9,5 +9,25 @@
                 Updates</span>
 
         </div>
+        <div class="flex sm:justify-between flex-wrap items-center pt-5">
+            <div class="flex gap-2">
+                <button class="bg-blue-600 text-white px-3 py-2 rounded-xl text-sm">CULTURE</button>
+                <button class="bg-blue-600 text-white px-3 py-2 rounded-xl text-sm">WADE WARREN</button>
+            </div>
+            <div class="flex gap-3 items-center">
+                <span class="text-sm">Sep 9, 2024</span>
+                <span class="text-xl">-</span>
+                <span class="text-sm"> 06 Minute</span>
+            </div>
+        </div>
+        <div class="flex justify-between items-start pt-5">
+            <div class="text-4xl w-[75%] font-bold">
+                A deep dive into the influence of cultural movements on contemporary society
+            </div>
+            <div class="flex gap-2 items-center cursor-pointer">
+                <span class="text-sm"> Read Article</span> <i
+                    class="fa-solid fa-arrow-right border border-solid border-gray-950 rounded-full p-1"></i>
+            </div>
+        </div>
     </div>
 </section>
