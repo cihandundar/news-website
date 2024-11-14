1478e04ef30217a011fe816694b8cfae0c550366
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-11-14 22:07:07 +0300

    hero section added

--- resources/views/front/pages/home.blade.php
+++ resources/views/front/pages/home.blade.php
@@ -3,4 +3,5 @@
 
 @section('content')
     @include('front.partials.sections.news-category')
+    @include('front.partials.sections.hero')
 @endsection
---
+++ resources/views/front/partials/sections/hero.blade.php
@@ -0,0 +1,13 @@
+<section>
+    <div class="container max-w-screen-xl mx-auto">
+        <div class="relative">
+            <img src="{{ asset('front/assets/images/hero.jpg') }}" alt="hero">
+
+            <span class="absolute top-5 left-5 bg-white px-3 py-1 flex items-center gap-2 shadow-lg font-bold"> <span
+                    class="h-3 w-3 bg-red-600 inline-block rounded-full "></span>
+                Live
+                Updates</span>
+
+        </div>
+    </div>
+</section>
