118717f8ef6178865a024e1edd174f86d1cc7d08
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-11-15 22:00:46 +0300

    animation added

--- resources/views/front/partials/sections/hero.blade.php
+++ resources/views/front/partials/sections/hero.blade.php
@@ -4,8 +4,13 @@
             <div class="relative">
                 <img src="{{ asset('front/assets/images/hero.jpg') }}" alt="hero">
 
-                <span class="absolute top-5 left-5 bg-white px-3 py-1 flex items-center gap-2 shadow-lg font-bold"> <span
-                        class="h-3 w-3 bg-red-600 inline-block rounded-full "></span>
+                <span
+                    class="absolute top-5 left-5 bg-white px-3 py-1 flex items-center gap-2 shadow-lg font-medium"><span
+                        class="relative flex h-3 w-3">
+                        <span
+                            class="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-600 opacity-75"></span>
+                        <span class="relative inline-flex rounded-full h-3 w-3 bg-red-600"></span>
+                    </span>
                     Live
                     Updates</span>
 
