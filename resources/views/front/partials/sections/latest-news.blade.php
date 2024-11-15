ca3659e4615bbd26f06bdda590d903df7d2eb8ed
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-11-16 01:34:23 +0300

    responsive fix

--- resources/views/front/partials/sections/latest-news.blade.php
+++ resources/views/front/partials/sections/latest-news.blade.php
@@ -1,81 +1,66 @@
 <section class="py-[90px]">
     <div class="container max-w-screen-xl mx-auto px-3">
-        <div class="title""> Latest News</div>
-        <div class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 pt-5">
-            <div class="md:col-span-1 row-span-2">
-                <a href="#" class="relative">
+        <div class="title">Latest News</div>
+        <div class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 pt-5 grid-flow-row">
+
+            <div class="md:col-span-1 md:row-span-2 col-span-3 w-full">
+                <a href="#" class="relative w-full">
                     <img class="w-full h-full object-cover" src="{{ asset('front/assets/images/latest1.jpg') }}"
                         alt="news">
-
-                    <div class="absolute flex flex-col bottom-16 left-5 right-5 text-white bg-blue-600 p-3">
-                        <span class="text-xl">Riding the tide of innovation and inspiration</span>
+                    <div class="absolute flex flex-col bottom-4 left-5 right-5 text-white bg-blue-600 p-3">
+                        <span class="text-xl font-semibold">Riding the tide of innovation and inspiration</span>
                         <span>Bussines - Sep 6, 2024</span>
                     </div>
-
                 </a>
             </div>
 
             <a href="#"
-                class="col-span-2 flex items-center gap-5 hover:shadow-lg p-3 transition-all ease-in duration-300">
+                class="col-span-2 flex md:items-center md:flex-row flex-col gap-5 hover:shadow-lg p-3 transition-all ease-in duration-300">
                 <div class="flex flex-col flex-1">
-                    <div>
-                        The effects of geopolitical shifts on global security
-                    </div>
-                    <div class="pt-2 text-sm text-gray-600">
-                        World News - Sep 9, 2024
-                    </div>
+                    <div class="text-lg font-semibold">The effects of geopolitical shifts on global security</div>
+                    <div class="pt-2 text-sm text-gray-600">World News - Sep 9, 2024</div>
                 </div>
-                <img class="w-2/4 h-full object-cover" src="{{ asset('front/assets/images/latest2.jpg') }}"
-                    alt="news">
+                <img class="w-full md:w-1/3 w-3/3 md:h-full object-cover"
+                    src="{{ asset('front/assets/images/latest2.jpg') }}" alt="news">
             </a>
 
             <a href="#"
-                class="col-span-2 flex items-center gap-5 hover:shadow-lg p-3 transition-all ease-in duration-300">
-                <div class="flex flex-col flex-1 ">
-                    <div>
-                        The effects of geopolitical shifts on global security
-                    </div>
-                    <div class="pt-2 text-sm text-gray-600">
-                        World News - Sep 9, 2024
-                    </div>
+                class="col-span-2 flex items-center md:flex-row flex-col gap-5 hover:shadow-lg p-3 transition-all ease-in duration-300">
+                <div class="flex flex-col flex-1">
+                    <div class="text-lg font-semibold">Strategies for success in a competitive landscape</div>
+                    <div class="pt-2 text-sm text-gray-600">World News - Sep 9, 2024</div>
                 </div>
-                <img class="w-2/4 h-full object-cover" src="{{ asset('front/assets/images/latest3.jpg') }}"
+                <img class="w-full md:w-1/3 w-3/3 md:h-full object-cover"
+                    src="{{ asset('front/assets/images/latest3.jpg') }}" alt="news">
+            </a>
+
+
+            <a href="#" class="hover:shadow-xl transition-all duration-300 md:col-span-1 col-span-3 ">
+                <img class="w-full h-48 object-cover" src="{{ asset('front/assets/images/latest4.jpg') }}"
                     alt="news">
-            </a>
-
-
-            <a href="#" class="hover:shadow-xl transition-all duration-300">
-                <img src="{{ asset('front/assets/images/latest4.jpg') }}" alt="news">
                 <div class="p-3">
-                    <div class="text-lg">Innovation hubs are transforming traditional business models</div>
-                    <div>
-                        Bussiness - Sep 9, 2024
-                    </div>
-                </div>
-            </a>
-            <a href="#" class="hover:shadow-xl transition-all duration-300">
-                <img src="{{ asset('front/assets/images/latest5.jpg') }}" alt="news">
-                <div class="p-3">
-                    <div class=" text-lg">Tailoring treatments to individual genetic profiles</div>
-                    <div>
-                        Health - Sep 9, 2024
-                    </div>
-                </div>
-            </a>
-            <a href="#" class="hover:shadow-xl transition-all duration-300">
-                <img src="{{ asset('front/assets/images/latest6.jpg') }}" alt="news">
-
-                <div class="p-3">
-                    <div class="text-lg">Strategies for success in a competitive landscape</div>
-                    <div>
-                        Bussiness - Sep 9, 2024
-                    </div>
+                    <div class="text-lg font-semibold">Innovation hubs transforming business models</div>
+                    <div class="text-sm text-gray-600">Bussines - Sep 9, 2024</div>
                 </div>
             </a>
 
+            <a href="#" class="hover:shadow-xl transition-all duration-300 md:col-span-1 col-span-3 ">
+                <img class="w-full h-48 object-cover" src="{{ asset('front/assets/images/latest5.jpg') }}"
+                    alt="news">
+                <div class="p-3">
+                    <div class="text-lg font-semibold">Tailoring treatments to individual genetic profiles</div>
+                    <div class="text-sm text-gray-600">Health - Sep 9, 2024</div>
+                </div>
+            </a>
 
-
+            <a href="#" class="hover:shadow-xl transition-all duration-300 md:col-span-1 col-span-3 ">
+                <img class="w-full h-48 object-cover" src="{{ asset('front/assets/images/latest6.jpg') }}"
+                    alt="news">
+                <div class="p-3">
+                    <div class="text-lg font-semibold">Strategies for success in a competitive landscape</div>
+                    <div class="text-sm text-gray-600">Bussines - Sep 9, 2024</div>
+                </div>
+            </a>
         </div>
-
     </div>
 </section>
