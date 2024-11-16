49950460dc0cb7ca3e2c0f3654136a37d7795787
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-11-16 14:02:38 +0300

    hover effect added

--- resources/views/front/partials/sections/writers.blade.php
+++ resources/views/front/partials/sections/writers.blade.php
@@ -2,22 +2,28 @@
     <div class="container max-w-screen-xl mx-auto px-3">
         <div class="title">staff wrıters</div>
         <div class="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 pt-5 ">
-            <a href="#" class="relative flex justify-center hover:shadow-xl transition-all duration-300">
-                <img src="{{ asset('front/assets/images/writers.jpg') }}" alt="news-image">
+            <a href="#"
+                class="relative flex justify-center hover:shadow-xl transition-all duration-300 overflow-hidden">
+                <img class="lg:hover:scale-105 transition-all duration-300"
+                    src="{{ asset('front/assets/images/writers.jpg') }}" alt="news-image">
                 <div class="absolute bottom-10 bg-white px-10 py-3"><span class="font-medium text-lg"> Theresa Webb
                     </span> -
                     <span class="text-gray-600">Publisher</span>
                 </div>
             </a>
-            <a href="#" class="relative flex justify-center hover:shadow-xl transition-all duration-300">
-                <img src="{{ asset('front/assets/images/writers2.jpg') }}" alt="news-image">
+            <a href="#"
+                class="relative flex justify-center hover:shadow-xl transition-all duration-300 overflow-hidden">
+                <img class="lg:hover:scale-105 transition-all duration-300"
+                    src="{{ asset('front/assets/images/writers2.jpg') }}" alt="news-image">
                 <div class="absolute bottom-10 bg-white px-10 py-3"><span class="font-medium text-lg"> Marvin McKinney
                     </span> -
                     <span class="text-gray-600">Senior Reporter</span>
                 </div>
             </a>
-            <a href="#" class="relative flex justify-center hover:shadow-xl transition-all duration-300">
-                <img src="{{ asset('front/assets/images/writers3.jpg') }}" alt="news-image">
+            <a href="#"
+                class="relative flex justify-center hover:shadow-xl transition-all duration-300 overflow-hidden">
+                <img class="lg:hover:scale-105 transition-all duration-300"
+                    src="{{ asset('front/assets/images/writers3.jpg') }}" alt="news-image">
                 <div class="absolute bottom-10 bg-white px-10 py-3"><span class="font-medium text-lg"> Guy Hawkins
                     </span> -
                     <span class="text-gray-600">Content Writer</span>
